b99f4beafa7042e6918b440352f41412674463b4
Yana <yanina.dolzhenko@gmail.com>
2023-04-25 23:00:19 +0400

    add methods

--- Tests/TestUserController.php
+++ Tests/TestUserController.php
@@ -1,1 +1,25 @@
 <?php
+class TestUserController
+{
+    public function testLogin($login)
+    {
+
+        if(strlen($login)>3 && strlen($login)<33 )
+        {
+            return true;
+        }
+
+        return false;
+    }
+    public function testPassword($pass)
+    {
+
+        if(strlen($pass)>3 && strlen($pass)<33 )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
